d3e0189dd51821143be547f9a5222688ca3820b5
Mutero10 <mutero.27.andrew@gmail.com>
2025-03-26 14:16:51 +0300

    Added HTTP response to display current date and time

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class HttpServer {
     
@@ -14,14 +15,9 @@ public class HttpServer {
     System.out.println("Listening for connections on port 8080...");
         
     while (true) {
-      Socket clientSocket = server.accept();
-      InputStreamReader isr = new InputStreamReader (clientSocket.getInputStream());
-      BufferedReader reader = new BufferedReader(isr);
-      
-      String line = reader.readLine();
-      while (!line.isEmpty()) {
-          System.out.println(line);
-          line = reader.readLine();
+    try(Socket socket = server.accept()) {
+      Date today = new Date();
+      String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today; socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));         
         }
       }
     }
