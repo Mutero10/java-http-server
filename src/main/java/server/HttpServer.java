d1344251f00c1e281e2c1fbd4df687da48226f62
Mutero10 <mutero.27.andrew@gmail.com>
2025-03-26 14:03:01 +0300

    Implemented basic HTTP request handling and logging

--- src/main/java/server/HttpServer.java
+++ src/main/java/server/HttpServer.java
@@ -1,13 +1,28 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class HttpServer {
+    
     public static void main(String[] args) throws Exception {
-        final ServerSocket server = new ServerSocket(8080);
-        System.out.println("Listening for connections on port 8080...");
         
-        while (true) {
+    final ServerSocket server = new ServerSocket(8080);
+    System.out.println("Listening for connections on port 8080...");
+        
+    while (true) {
+      Socket clientSocket = server.accept();
+      InputStreamReader isr = new InputStreamReader (clientSocket.getInputStream());
+      BufferedReader reader = new BufferedReader(isr);
+      
+      String line = reader.readLine();
+      while (!line.isEmpty()) {
+          System.out.println(line);
+          line = reader.readLine();
         }
+      }
     }
 }
